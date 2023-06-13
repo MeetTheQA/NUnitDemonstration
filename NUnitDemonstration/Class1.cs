b4dc0a9e2b994215f267d2a37ba9a68dbfb60160
sheth <sheth@THE_MEETSHETH>
2023-06-13 18:48:23 -0400

    Add project files.

---
+++ NUnitDemonstration/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace NUnitDemonstration
+{
+// Meet Sheth's Source Code
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 10)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ TryingAnnotations/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace TryingAnnotations
+{
+   
+    public class TryingAnnotations
+    {
+        [TestFixture]
+        public class TryingAnnotations1
+        {
+            [Test]
+
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            // Meet Sheth's Test
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                Assert.AreEqual(expected, actual);
+
+            }
+
+        }
+       
+    }
+}
